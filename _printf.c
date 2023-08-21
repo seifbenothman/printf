f2ea0802ea568765eb0c37f25d81bc62d3c71c7f
seifbenothman <seifbenothman95@gmail.com>
2023-08-21 10:36:40 +0000

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* _printf - Custom printf function
+ * @format: The format string
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count;
+
+	va_start(args, format);
+
+	count = count_format(format);
+
+	va_end(args);
+
+	return (count);
+}
